c22ed622501f730c3b1ba6dc89558f8bbeb61eea
Robin Sundman Nilsson <sundmanrobin@outlook.com>
2025-09-13 14:52:51 +0200

    setup new dbt

--- 10_dbt_setup/dbt_code/macros/translate_headline.sql
+++
@@ -1,6 +0,0 @@
-{% macro translate_headline(column_name) %}
-    CASE 
-        WHEN {{ column_name }} = 'Data Engineer' THEN 'Junior Data Engineer'
-        ELSE {{ column_name }}
-    END
-{% endmacro %}
---
+++ 10_dbt_setup/logs/query_log.sql
@@ -0,0 +1,5 @@
+-- created_at: 2025-09-13T12:47:14.281173+00:00
+-- dialect: snowflake
+-- node_id: not available
+-- desc: not provided
+select 1 as id;
